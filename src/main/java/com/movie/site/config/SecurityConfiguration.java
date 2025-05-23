4dc8fe342d7f3ba2bec07a2546d10caccf230ba3
Arber <arber.kulla888@gmail.com>
2025-05-23 08:14:44 +0200

    Swagger

--- src/main/java/com/movie/site/config/SecurityConfiguration.java
+++ src/main/java/com/movie/site/config/SecurityConfiguration.java
@@ -23,7 +23,13 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-    private static final String[] WHITE_LIST_URL = {"/api/auth/**","/api/posts/get"};
+    private static final String[] WHITE_LIST_URL = {
+            "/api/auth/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/error"
+    };
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
